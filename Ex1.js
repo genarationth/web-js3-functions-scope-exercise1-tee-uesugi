9c1f7e1c7ffdb485ac598eeafe1ad9ab33659c60
04_Apiwat (Tee/ตี๋) <apiwat.pienwisetkaew@gmail.com>
2024-05-14 13:43:49 +0700

    first-attemp

---
+++ Ex1.js
@@ -0,0 +1,17 @@
+//Exercise 1.1 Create function following below//
+
+
+const add = (a,b) => a+b;
+
+const subtract = (a,b) => a-b;
+
+const multiply = (a,b) => a*b;
+
+const divide = (a,b) => a/b;
+
+
+//Exercise 1.2 Create Function Inform//
+
+const inform = (name, location, hobbie) => console.log(`Hi, My name is ${name}, I live in ${location} and enjoy ${hobbie}.`);
+
+inform('tee','japan','sleep');
